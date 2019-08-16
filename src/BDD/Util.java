90f2c73f7dae897721aa81cea09772fc9c162b84
Wenyuan Ma <wm263@cornell.edu>
2019-08-15 22:27:41 -0700

    add BDDnode class

---
+++ src/BDD/Util.java
@@ -0,0 +1,18 @@
+package BDD;
+
+import java.util.HashMap;
+
+public class Util {
+    /*
+    public static BDDnode<A,B> buildBDDnode(f: 2^A -> B) {
+
+    }
+
+
+    public static BDDnode<A,B> apply(f:B*B -> B,
+                                     BDDnode<A,B> x,
+                                     BDDnode<A,B> y) {
+
+    }
+    */
+}
