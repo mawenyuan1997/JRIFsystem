3b55a0ec744647a03a28739aef0956a782a1e5b8
Wenyuan Ma <wm263@cornell.edu>
2019-09-15 20:25:48 -0400

    add epsilon function for SyKAT

---
+++ src/SyKAToperator/Epsilon.java
@@ -0,0 +1,52 @@
+package SyKAToperator;
+
+import SyKAT.BDD.BDD;
+import SyKAT.BDD.BooleanFunction;
+import SyKAT.BDD.BooleanOperator;
+import SyKAT.Concat;
+import SyKAT.Plus;
+import SyKAT.Primitive;
+import SyKAT.Star;
+
+public class Epsilon implements SyKATexpressionVisitor {
+    @Override
+    public Object visit(BDD expr) {
+        return new BDD(expr);
+    }
+
+    @Override
+    public Object visit(Concat expr) {
+        BDD l = (BDD) expr.left.accept(this);
+        BDD r = (BDD) expr.right.accept(this);
+        BooleanOperator op = new BooleanOperator() {
+            @Override
+            public boolean operate(boolean x, boolean y) {
+                return x && y;
+            }
+        };
+        return new BDD(op, l, r);
+    }
+
+    @Override
+    public Object visit(Plus expr) {
+        BDD l = (BDD) expr.left.accept(this);
+        BDD r = (BDD) expr.right.accept(this);
+        BooleanOperator op = new BooleanOperator() {
+            @Override
+            public boolean operate(boolean x, boolean y) {
+                return x || y;
+            }
+        };
+        return new BDD(op, l, r);
+    }
+
+    @Override
+    public Object visit(Primitive expr) {
+        return new BDD(BDD.Function.FALSE);
+    }
+
+    @Override
+    public Object visit(Star expr) {
+        return new BDD(BDD.Function.TRUE);
+    }
+}
